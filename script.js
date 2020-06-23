54f1ad3b7edea29d01117e625fb1c8f0efc05db9
Walison <walyteodoro@gmail.com>
2020-06-23 10:39:32 -0300

    game complete

--- script.js
+++ script.js
@@ -7,35 +7,80 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var score, roundScore, activePlayer, dice
+var scores, roundScore, activePlayer, dice, gamePlaying
 
-score = [0,0]
-roundScore = 0
-activePlayer = 0
-
-// Defaut
-document.querySelector('.dice').style.display = 'none'
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+init()
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    // 1. Random number
-    dice = Math.floor(Math.random()* 6 ) + 1
+    // the condition of gamePlaying is true or false, so i can use gamePlaying or !gamePlaying
+    if (gamePlaying) {
+        // 1. Random number
+        dice = Math.floor(Math.random()* 6 ) + 1
 
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice')
-    diceDOM.style.display = 'block'
-    diceDOM.src = 'image/dice-' + dice + '.png'
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice')
+        diceDOM.style.display = 'block'
+        diceDOM.src = 'image/dice-' + dice + '.png'
 
-    //3. Update the round score IF the rolled number was NOT a 1
-    if (dice !== 1) {
-        // add score
-        roundScore += dice
-        document.querySelector('#current-' + activePlayer).textContent = roundScore
-    } else {
-        //Nex player
+        //3. Update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            // add score
+            roundScore += dice
+            document.querySelector('#current-' + activePlayer).textContent = roundScore
+        } else {
+            nextPlayer()
+        }  
+    }
+    
+})
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    if (gamePlaying) {
+        //add current score to global score
+        scores[activePlayer] += roundScore
+        //update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+        //check if won the game
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+            document.querySelector('.dice').style.display = 'none'
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+            gamePlaying = false
+        } else {
+        //next player
+        nextPlayer()  
+        }  
+    }
+    
+})
+
+document.querySelector('.btn-new').addEventListener('click', init)
+
+function init() {
+    scores = [0,0]
+    roundScore = 0
+    activePlayer = 0
+    gamePlaying = true
+
+    // Defaut
+    document.querySelector('.dice').style.display = 'none'
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    document.getElementById('name-0').textContent = 'Player 1'
+    document.getElementById('name-1').textContent = 'Player 2'
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.add('active')
+    
+}
+
+function nextPlayer() {
+    //Nex player
         /* if is is 0 than active player should be 1 else activeplayer should be 0
         same as: if(activePlayer ===0){
             activePlayer = 1
@@ -51,14 +96,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('.player-0-panel').classList.toggle('active')
         document.querySelector('.player-1-panel').classList.toggle('active')
         //document.querySelector('.dice').style.display = 'none'
-    }
-})
+}
 
 
-
-/*The dice expression is for generate a random number between 1 and 6. Math.floor is to arround the number if no decimal, times 6 is because Math.random generates a random number
-between 0 and 1 and plus 1 is for never giva a 0 number on random. Im thinking if this ever could give a number 7....
-*/
 
 
 //document.querySelector('#current-0').textContent = dice
